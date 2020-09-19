6506d981f4952340ad18627fb85edffe07c06260
theooprea <opreatheodor54@gmail.com>
2020-09-19 13:31:26 +0300

    form

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the ReservationPage page.
@@ -15,15 +16,28 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ReservationPage {
 
+  reservation: FormGroup;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder) {
+      this.reservation = this.formBuilder.group({
+        guests: 3,
+        smoking: false,
+        dateTime: ['', Validators.required],
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReservationPage');
   }
-  
+
   dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  onSubmit() {
+    console.log(this.reservation.value);
     this.viewCtrl.dismiss();
   }
 }
